8c3983cd2a85643cce58904b65c521e24f6c1646
AJ Bahnken <aj@ajvb.me>
2019-11-07 09:53:05 -0800

    v2

--- anotherfile.go
+++ anotherfile.go
@@ -2,6 +2,7 @@ package modules_testing_sops
 
 import "fmt"
 
-func AnotherExampleFunc(msg string) {
+func AnotherExampleFunc(msg, secondMsg string) {
 	fmt.Println(msg)
+	fmt.Println(secondMsg)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module modules-testing-sops
+module modules-testing-sops/v2
 
 go 1.13
